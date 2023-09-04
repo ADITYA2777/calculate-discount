d556f38d13011b41d718b2fafd9e789ab9ddf628
ADITYA2777 <ajadityajain01@gmail.com>
2023-09-04 13:12:48 -0700

    calculate -discount

---
+++ index.js
@@ -0,0 +1,22 @@
+
+
+const audio = new Audio("./alert.mp3")
+const discountBtn = document.getElementById("dis-btn");
+discountBtn.addEventListener(("click", () => {
+    let OriginalPrice = document.querySelector("#actualPrice").value
+    let DiscountPrice = document.querySelector("#actualDiscount").value;
+
+    if (OriginalPrice ===""|| DiscountPrice ==="") {
+        audio.play();
+        alert("please enter all the input field")
+    }
+    else {
+        const discount = OriginalPrice - DiscountPrice;
+        const percentageDiscount = 100 * (Number(discount/OriginalPrice));
+        document.querySelector(".output").innerHTML =
+            `Congrualtions 🚀 aditya jain  you got <span class="DiscountPercentage">
+            ${percentageDiscount.toFixed(2)}</span>
+    discount on  this product`;
+        document.querySelector(".img-gif").style.display="flex";
+    }
+}));
